e63a5d95add46183da958dc3d806e3023eed7a36
oleg.kuzmenko <oleg.kuzmenko@witte-automotive.cz>
2025-05-22 12:03:22 +0200

    update

--- src/DependencyInjection/SyDataGridExtension.php
+++ src/DependencyInjection/SyDataGridExtension.php
@@ -1,11 +1,13 @@
 <?php
 namespace SyDataGrid\DependencyInjection;
+
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
-class SyDataGridExtension extends Extension
+class SyDataGridExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container)
     {
