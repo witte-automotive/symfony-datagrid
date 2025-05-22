23a87afbdcfe11647584a9c665d58336c7244d3b
oleg.kuzmenko <oleg.kuzmenko@witte-automotive.cz>
2025-05-22 11:10:44 +0200

    update

--- src/Template/GridTemplate.php
+++ src/Template/GridTemplate.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace SyDataGrid\Template;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-
-#[AsTwigComponent('SyDataGrid', 'grid/grid.html.twig')]
+#[AsTwigComponent(name: 'SyDataGrid', template: '@SyDataGrid/grid/grid.html.twig')]
 class GridTemplate
 {
-
+    public mixed $grid;
 }
