1794cf9b46ea365bca0c6f60480c5e385b12cf9a
oleg.kuzmenko <oleg.kuzmenko@witte-automotive.cz>
2025-05-22 13:06:06 +0200

    refactor

--- src/SyDataGridBundle.php
+++ src/SyDataGridBundle.php
@@ -1,8 +1,20 @@
 <?php
 namespace SyDataGrid;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-class SyDataGridBundle extends Bundle
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+class SyDataGridBundle extends AbstractBundle
 {
-
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $builder->prependExtensionConfig('twig_component', [
+            'defaults' => [
+                'SyDataGrid\Twig\Component\\' => [
+                    'template_directory' => '@SyDataGrid/components/',
+                    'name_prefix' => 'SyDataGrid',
+                ],
+            ],
+        ]);
+    }
 }
---
+++ src/Twig/Component/SyDataGrid.php
@@ -0,0 +1,4 @@
+<?php
+
+namespace App\Twig\Component;
+
