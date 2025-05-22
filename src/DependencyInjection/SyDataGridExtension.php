f2953a32949dee0dfe63521e1df1eb742afd81e1
oleg.kuzmenko <oleg.kuzmenko@witte-automotive.cz>
2025-05-22 12:34:56 +0200

    update

--- src/DependencyInjection/SyDataGridExtension.php
+++ src/DependencyInjection/SyDataGridExtension.php
@@ -3,26 +3,14 @@ namespace SyDataGrid\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
-class SyDataGridExtension extends Extension implements PrependExtensionInterface
+class SyDataGridExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
-    }
-
-    public function prepend(ContainerBuilder $container)
-    {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $config = $loader->load('twig.yaml');
-
-        $yamlParser = new \Symfony\Component\Yaml\Parser();
-        $twigConfig = $yamlParser->parse(file_get_contents(__DIR__ . '/../Resources/config/twig.yaml'));
-
-        $container->prependExtensionConfig('twig', $twigConfig);
     }
 }
