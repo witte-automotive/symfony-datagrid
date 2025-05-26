381ffe90d28ce42dd875e971f484c696a276b19a
oleg.kuzmenko <oleg.kuzmenko@witte-automotive.cz>
2025-05-26 09:11:17 +0200

    update

--- src/Resources/public/assets/sydatagrid.js
+++ src/Resources/public/assets/sydatagrid.js
@@ -1,1 +1,1 @@
-import{D as t}from"./js/DataGrid/DataGrid-Du1pPq_L.js";import"./js/Service/Ajax-i33u5mVt.js";document.querySelectorAll(".js-sydatagrid").forEach(r=>{new t().init(r)});
+import{D as t}from"./js/DataGrid/DataGrid-8eHkW1kN.js";import"./js/Service/Ajax-i33u5mVt.js";document.querySelectorAll(".js-sydatagrid").forEach(r=>{new t().init(r)});
